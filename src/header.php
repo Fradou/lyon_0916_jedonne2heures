e84aa3559b3e784034cc5baa5ff38ac934ca9b29
Matthieu Janvier <janvierm@gmail.com>
2016-09-28 11:37:54 +0200

    base header.php

---
+++ src/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <title>Je donne 2 heures</title>
+
+</head>
+
+<style>
+  .nav-social {
+    display: flex;
+    flex-direction: row;
+  }
+
+  img {
+    height: 40px;
+  }
+</style>
+
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Je donne 2 heures</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Comment ça marche</a></li>
+          <li><a href="#">Qui sommes-nous</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right nav-social">
+          <li><a href="#"><img src="img/facebook.png" alt=""></a></li>
+          <li><a href="#"><img  src="img/twitter.png" alt=""></a></li>
+          <li><a href="#"><img  src="img/linkedin.png" alt=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<body>
