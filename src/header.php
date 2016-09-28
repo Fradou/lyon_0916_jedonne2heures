c2332e17aed3c5f62c1319e869d1a6b6cbcfeae0
Matthieu Janvier <janvierm@gmail.com>
2016-09-28 15:36:03 +0200

    include links.php

--- src/header.php
+++ src/header.php
@@ -3,12 +3,9 @@
 
 <head>
   <meta charset="utf-8">
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-  <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <?php
+    include 'inc/links.php';
+  ?>
   <title>Je donne 2 heures</title>
 
 </head>
