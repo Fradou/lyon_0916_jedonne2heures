6225a52ef9237829543e013d3bdb33fdef3d9d7b
Alexandre FRADET <a_fradet@yahoo.fr>
2016-10-03 08:52:43 +0200

    Add class & id for most pages. Create main.css. Delete some of the useless styling

--- src/jedonne.php
+++ src/jedonne.php
@@ -18,6 +18,7 @@ include 'include/header.php';
 	<style>
 		div {
 			height:20vh;
+			border:5px solid black;
 		}
 	</style>
 </head>
@@ -26,34 +27,32 @@ include 'include/header.php';
 
 	<div class="container-fluid">
 
-		<div class="row" style="border:5px solid black">Empty row de transition </div>
+		<div class="row all_blsp">Empty row de transition </div>
 
 		<div class="row">
-			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="img_ambiance" style="background-color:blue"> "Image d'ambiance"</div>
+			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="img_ambiance"> "Image d'ambiance"</div>
 		</div>
 
-		<div class="row" style="border:5px solid black">Empty row de transition </div>
+		<div class="row all_blsp">Empty row de transition </div>
 
 		<div class="row" style="height:60vh">
-			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="img_ambiance" style="height:100%; background-color:blue"> "Bloc de texte presentation."</div>
+			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="img_ambiance" style="height:100%"> "Bloc de texte presentation."</div>
 		</div>
 
-		<div class="row" style="border:5px solid black">Empty row de transition </div>
+		<div class="row all_blsp">Empty row de transition </div>
 
-		<div class="row" style="height:80vh">
-			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="img_ambiance" style="height:100%; background-color:blue"> "Formulaire de souscription"</div>
+		<div class="row how_form_ext">
+			<div class="how_form_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6"> "Formulaire de souscription"</div>
 		</div>
 
-		<div class="row" style="border:5px solid black">Empty row de transition </div>
+		<div class="row all_blsp">Empty row de transition </div>
 
 	</div>
-
-</body>
-
-</html>
-
-
 
 <?php
 include 'include/footer.php';
 ?>
+
+</body>
+
+</html>
