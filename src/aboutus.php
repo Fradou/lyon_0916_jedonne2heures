a57e46aefe13aa947279205df7d1a48ef4d1df73
Matthieu Janvier <janvierm@gmail.com>
2016-10-04 11:17:09 +0200

    correction path to social images

--- src/aboutus.php
+++ src/aboutus.php
@@ -2,35 +2,13 @@
 $pageTitle = "Qui sommes nous ?";
 include_once 'inc/header.php';
 ?>
+    <!--<style>
+        div {
+            height:20vh;
+            border:5px solid black;
+        }
+    </style>-->
 
-<!DOCTYPE html>
-
-<html>
-
-<head>
- 	<!--jquery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
- 	<script src="https://code.jquery.com/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk=" crossorigin="anonymous"></script>
- 	<!-- bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<meta http-equiv="Content-type" content="text/html" charset="utf-8" />
-	<!--<style>
-		div {
-			height:20vh;
-			border:5px solid black;
-		}
-	</style>-->
-</head>
-
-<body>
-
-=======
-include_once '../src/inc/header.php';
-?>
-
->>>>>>> 879e2b4eddc06ad4f36a9209e159d942ac336e80
 	<div class="container-fluid">
 
 		<div class="row all_blsp">Empty row de transition </div>
