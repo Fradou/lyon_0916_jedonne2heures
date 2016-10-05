950e2a3e3b005bd40186043ffabebba928178a49
AymenMech <aymen.mechiche@gmail.com>
2016-10-05 15:46:32 +0200

    commit before pull

--- public/index.php
+++ public/index.php
@@ -39,23 +39,58 @@ A voir : faire deux niveaux de bootstrap pour gerer le medium/ large et faire de
 
 !! Offset diffère un peu suivant la gestion des offset au milieu des partenaires -->
 
-			<a href="https://www.mutum.com" target="_blank"><div id="home_blimg_startup1" class="home_blimg_startup col-xs-offset-2 col-xs-8 col-sm-offset-1 col-sm-4 col-md-offset-2 col-md-2" > Partnr 1 - modèle base
+			<a href="https://www.mutum.com" target="_blank">
+				<div id="home_blimg_startup1" class="home_blimg_startup col-xs-offset-2 col-xs-8 col-sm-offset-1 col-sm-4 col-md-offset-2 col-md-2" >
+					<div class="test">
+						<img src="../../logotikaway.png">
+						<h5>Tikaway</h5>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://www.foodandyou.fr/" target="_blank">
+				<div id="home_blimg_startup2" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-2 col-sm-4 col-md-offset-1 col-md-2" >
+					<div class="test">
+						<img src="../../logotikaway.png">
+						<h5>Tikaway</h5>
+					</div>
 			</div></a>
 
-			<a href="https://www.foodandyou.fr/" target="_blank"><div id="home_blimg_startup2" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-2 col-sm-4 col-md-offset-1 col-md-2" > Partnr 2 - offspé a sm et md+
-			</div></a>
+			<a href="https://commeunearmoire.com/fr" target="_blank">
+				<div id="home_blimg_startup3" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-1 col-sm-4 col-md-offset-1 col-md-2" >
+					<div class="test">
+						<img src="../../logotikaway.png">
+						<h5>Tikaway</h5>
+					</div>
+				</div>
+			</a>
 
-			<a href="https://commeunearmoire.com/fr" target="_blank"><div id="home_blimg_startup3" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-1 col-sm-4 col-md-offset-1 col-md-2" > Partnr 3 - offspé à md+
-			</div></a>
+			<a href="https://www.tikaway.com/fr/" target="_blank">
+				<div id="home_blimg_startup4" class="home_blimg_startup col-xs-offset-2 col-xs-8 col-sm-offset-2 col-sm-4 col-md-offset-2 col-md-2" >
+					<div class="test">
+						<img src="../../logotikaway.png">
+						<h5>Tikaway</h5>
+					</div>
+				</div>
+			</a>
 
-			<a href="https://www.tikaway.com/fr/" target="_blank"><div id="home_blimg_startup4" class="home_blimg_startup col-xs-offset-2 col-xs-8 col-sm-offset-2 col-sm-4 col-md-offset-2 col-md-2" ><div class="test"><img src="../../logotikaway.png"><h5>Tikaway</h5></div>
-			</div></a>
+			<a href="http://www.skware.com/" target="_blank">
+				<div id="home_blimg_startup5" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-1 col-sm-4 col-md-offset-1 col-md-2" >
+					<div class="test">
+						<img src="../../logotikaway.png">
+						<h5>Tikaway</h5>
+					</div>
+				</div>
+			</a>
 
-			<a href="http://www.skware.com/" target="_blank"><div id="home_blimg_startup5" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-1 col-sm-4 col-md-offset-1 col-md-2" > Partnr 5 - offspé à md+
-			</div></a>
-
-			<a href="http://www.2rives.tv/" target="_blank"><div id="home_blimg_startup6" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-2 col-sm-4 col-md-offset-1 col-md-2" > Partnr 6 - offspé à sm et md+
-			</div></a>
+			<a href="http://www.2rives.tv/" target="_blank">
+				<div id="home_blimg_startup6" class="home_blimg_startup  col-xs-offset-2 col-xs-8 col-sm-offset-2 col-sm-4 col-md-offset-1 col-md-2" >
+					<div class="test">
+						<img src="../../logotikaway.png">
+						<h5>Tikaway</h5>
+					</div>
+				</div>
+			</a>
 
 		</div>
 
