2ed4bee2770012818937cca83fe1887466853e95
lauriegandon <laurie.gandon@gmail.com>
2016-10-11 18:05:27 +0200

    add photos page about us

--- src/aboutus.php
+++ src/aboutus.php
@@ -11,27 +11,31 @@
 
 	<div class="container-fluid">
 
-		<div class="row all_blsp">Empty row de transition </div>
+		<div class="row all_blsp">
+            <div class=" who_bltxt col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="who_bltxt_int">
+                <h1>Qui sommes-nous</h1>
+            </div>
+        </div>
+
 
 		<div class="row who_blocimg">
-			<div class="col-xs-offset-1 col-xs-4 col-sm-offset-2 col-sm-3 col-md-offset-3 col-md-2" id="who_blocimg_pic1">
-				<div> Photo 1 </div>
+			<div class="col-xs-offset-3 col-xs-6 col-sm-offset-2 col-sm-3 col-md-offset-3 col-md-2" id="who_blocimg_pic1">
+				<img src="../public/img/je-donne-2-heures-guylaine-antonini.png" class="img-responsive" alt="Guylaine Antonini cofondatrice Je Donne 2 Heures">
 				<div class="who_blocsoc">
 					<h2>Guylaine Antonini</h2>
 					<a href="https://twitter.com/Guyylaine" target="_blank"><img  src="../public/img/twitter.png" alt=""></a>
 					<a href="https://fr.linkedin.com/in/guylaineantonini" target="_blank"><img  src="../public/img/linkedin.png" alt=""></a>
 				</div>
 			</div>
-			<div class="col-xs-offset-2 col-xs-4 col-sm-offset-2 col-sm-3 col-md-offset-2 col-md-2" id="who_blocimg_pic2">
-				<div> Photo 2 </div>
+			<div class="col-xs-offset-3 col-xs-6 col-sm-offset-2 col-sm-3 col-md-offset-2 col-md-2" id="who_blocimg_pic2">
+                <img src="../public/img/je-donne-2-heures-nicolas-antonini.png" class="img-responsive" alt="Nicolas Antonini cofondatrice Je Donne 2 Heures">
 				<div class="who_blocsoc">
 					<h2>Nicolas Antonini</h2>
 					<a href="https://twitter.com/nicolasantonini" target="_blank"><img  src="../public/img/twitter.png" alt=""></a>
 					<a href="https://fr.linkedin.com/in/nicolasantonini" target="_blank"><img  src="../public/img/linkedin.png" alt=""></a>
 				</div>
 			</div>
-
-			<div class="row all_blsp">Empty row de transition </div>
+        </div>
 
 			<div class="row who_bltxt" id="who_bltxt_ext">
 				<div class=" who_bltxt col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="who_bltxt_int">
@@ -41,9 +45,8 @@
 				</div>
 			</div>
 
-
 			<div class="row who_form_ext">
-				<div class="who_form_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="who_form"><h2>Vous avez des questions ? <br/>Vous voulez nous rencontrer ? <br/> Prendre une café </h2>
+				<div class="who_form_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="who_form"><h2>Vous avez des questions ? Vous voulez nous rencontrer, prendre un café ? <br/>Contactez-nous ! </h2>
 				</div>
 				<div class="who_form_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6">
 					<?php include_once 'inc/formaboutus.php'; ?>
