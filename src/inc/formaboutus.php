25a0fb40273e685c66c55a78db7fb0f46c81a963
lauriegandon <laurie.gandon@gmail.com>
2016-10-11 13:58:17 +0200

    add contact form + margin between form & footer about us page

--- src/inc/formaboutus.php
+++ src/inc/formaboutus.php
@@ -8,45 +8,22 @@
             <input type="text" class="form-control" name="nomcomplet" >
           </div>
           <div class="form-group">
-            <label for="Statut">Je suis</label>
-            <select name="statut" class="form-control">
-              <option>Etudiant</option>
-              <option>Salarié</option>
-              <option>Entre 2 jobs</option>
-              <option>Indépendant</option>
-              <option>A la retraite</option>
-              <option>Anarchiste</option>
-            </select>
+            <label for="Société">Société (facultatif)</label>
+            <input type="text" class="form-control" name="societe" >
           </div>
           <div class="form-group">
-            <label for="Temps à donner">Je désire donner</label>
-            <select name="tempsadonner" class="form-control">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="email">Votre email</label>
+            <label for="Email">Votre email</label>
             <input type="email" class="form-control"  name="email">
           </div>
           <div class="form-group">
-            <label for="Ville">Ville</label>
-            <input type="text" class="form-control" name="ville">
+            <label for="Sujet">Sujet</label>
+            <input type="text" class="form-control" name="sujet">
           </div>
           <div class="form-group">
-            <label for="Pays">Pays</label>
-            <select name="Pays" class="form-control">
-              <option>France</option>
-              <option>Europe</option>
-              <option>Reste du monde</option>
-            </select>
+              <label for="Message">Message</label>
+              <input type="text" class="form-control" name="message">
           </div>
+
           <div>
           <button type="submit" class="btn btn-default">Envoyer</button>
           </div>
