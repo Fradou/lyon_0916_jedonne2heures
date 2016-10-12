6006d9682e52d1b74734d32edcb51339c074ae2e
lauriegandon <laurie.gandon@gmail.com>
2016-10-12 22:05:21 +0200

    test social media icons page about us

--- src/aboutus.php
+++ src/aboutus.php
@@ -22,16 +22,28 @@
 				<img src="../public/img/je-donne-2-heures-guylaine-antonini.png" class="img-responsive who_img_portrait" alt="Guylaine Antonini cofondatrice Je Donne 2 Heures">
 				<div class="who_blocsoc">
 					<h2>Guylaine Antonini</h2>
-					<a href="https://twitter.com/Guyylaine" target="_blank"><img  src="../public/img/twitter.png" alt=""></a>
-					<a href="https://fr.linkedin.com/in/guylaineantonini" target="_blank"><img  src="../public/img/linkedin.png" alt=""></a>
+					<div class="who_twitter_icon">
+						<a href="https://twitter.com/Guylaine" target="_blank"><img src="../public/img/twitter-red.png"  class="twitter_top" alt="twitter black guylaine je donne 2 heures"></a>
+						<a href="https://twitter.com/Guylaine" target="_blank"><img src="../public/img/twitter-black.png" alt="Twitter guylaine Je Donne 2 Heures"></a>
+					</div>
+					<div class="who_linkedin_icon">
+						<a href="https://fr.linkedin.com/in/guylaineantonini" target="_blank"><img src="../public/img/linkedin-red.png"  class="linkedin_top" alt="linkedin"></a>
+						<a href="https://fr.linkedin.com/in/guylaineantonini" target="_blank"><img src="../public/img/linkedin-black.png" alt="Linkedin Guylaine Je Donne 2 Heures"></a>
+					</div>
 				</div>
 			</div>
 			<div class="col-xs-offset-3 col-xs-6 col-sm-offset-2 col-sm-3 col-md-offset-2 col-md-2" id="who_blocimg_pic2">
                 <img src="../public/img/je-donne-2-heures-nicolas-antonini.png" class="img-responsive who_img_portrait" alt="Nicolas Antonini cofondateur Je Donne 2 Heures">
 				<div class="who_blocsoc">
 					<h2>Nicolas Antonini</h2>
-					<a href="https://twitter.com/nicolasantonini" target="_blank"><img  src="../public/img/twitter.png" alt=""></a>
-					<a href="https://fr.linkedin.com/in/nicolasantonini" target="_blank"><img  src="../public/img/linkedin.png" alt=""></a>
+					<div class="who_twitter_icon">
+						<a href="https://twitter.com/nicolasantonini" target="_blank"><img src="../public/img/twitter-red.png"  class="twitter_top" alt="twitter black guylaine je donne 2 heures"></a>
+						<a href="https://twitter.com/nicolasantonini" target="_blank"><img src="../public/img/twitter-black.png" alt="Twitter Nicolas Je Donne 2 heures"></a>
+					</div>
+					<div class="who_linkedin_icon">
+						<a href="https://fr.linkedin.com/in/guylaineantonini" target="_blank"><img src="../public/img/linkedin-red.png"  class="linkedin_top" alt="linkedin"></a>
+						<a href="https://fr.linkedin.com/in/guylaineantonini" target="_blank"><img src="../public/img/linkedin-black.png" alt="Linkedin Nicolas Je Donne 2 Heures"></a>
+					</div>
 				</div>
 			</div>
 		</div>
