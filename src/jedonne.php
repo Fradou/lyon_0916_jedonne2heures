63a66dd992455bed51b2e3b7f0828eeaa4c6eeb0
Alexandre FRADET <a_fradet@yahoo.fr>
2016-10-12 14:28:19 +0200

    Delete an empty id

--- src/jedonne.php
+++ src/jedonne.php
@@ -35,7 +35,7 @@
 			</div>
 		</div>	
 
-		<div class="row all_blsp_sm" id=""></div>
+		<div class="row all_blsp_sm"></div>
 
 		<div class="row how_bloctxt_ext">
 			<div class="how_bloctxt_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="howp_bltxt">
