50573187bd4806e5f9306c6a32e83583f2e52e95
lauriegandon <laurie.gandon@gmail.com>
2016-10-12 16:12:55 +0200

    spelling mistake correction

--- src/jepropose.php
+++ src/jepropose.php
@@ -59,7 +59,7 @@
 			</div>
             <div class="how_form_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="howp_form">
                 <h3>Inscrivez-vous !<br>
-                    <small class="text-muted">Vous êtes intéressés pour déposer un projet ?<br>
+                    <small class="text-muted">Vous êtes intéressé pour déposer un projet ?<br>
                         Vous souhaitez vous tenir informé du lancement de <span>Je Donne 2 Heures</span> ?
                     </small>
                 </h3>
