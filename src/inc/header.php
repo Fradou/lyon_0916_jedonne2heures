dbc315779b9320d07ade773a49ef427914e9ca87
lauriegandon <laurie.gandon@gmail.com>
2016-10-12 20:30:38 +0200

    add new favicon & new logo

--- src/inc/header.php
+++ src/inc/header.php
@@ -8,7 +8,7 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="../public/index.php"><img src="../public/img/je-donne-2-heures-logo-header.png" alt="jd2h logo"></a>
+              <a class="navbar-brand" href="../public/index.php"><img src="../public/img/je-donne-2-heures-logo-header2.png" alt="jd2h logo"></a>
           </div>
           <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
