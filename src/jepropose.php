321beabde5e924cdc7cce160b6fb4d8a4f894033
lauriegandon <laurie.gandon@gmail.com>
2016-10-12 15:03:54 +0200

    change col md from 6 to 8

--- src/jepropose.php
+++ src/jepropose.php
@@ -14,10 +14,10 @@
 		<div class="row all_blsp_xs"></div>
 
 		<div class="row">
-			<div class="how_bloctxt_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="howp_bltxt"> 
+			<div class="how_bloctxt_int col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8" id="howp_bltxt">
 				<h1>Je propose un projet</h1>
 
-				<p>Vous êtes porteur d'un projet ? Vous êtes Entrepreneur et vous venez de démarrer ? Vous voulez faire grandir votre entreprise ? Vous avez des envies, des idées, des rêves et des besoins mais pas encore tous les moyens. <span>Je Donne 2 Heures</span> est fait pour Vous ! Grâce à notre plateforme, le monde entier est là pour vous apporter son soutien. Soyez les premiers à être tenu au courant du lancement de notre plateforme et donner vie à vos plus beaux projets.
+				<p>Vous êtes porteur d'un projet ? Vous êtes Entrepreneur et vous venez de démarrer ? Vous voulez faire grandir votre entreprise ? Vous avez des envies, des idées, des rêves et des besoins, mais pas encore tous les moyens. <span>Je Donne 2 Heures</span> est fait pour Vous ! Grâce à notre plateforme, le monde entier est là pour vous apporter son soutien. Soyez les premiers à être tenus au courant du lancement de notre plateforme et donner vie à vos plus beaux projets.
 				</p>
 			</div>
 		</div>
@@ -38,14 +38,14 @@
 		<div class="row all_blsp_sm"></div>
 
 		<div class="row how_bloctxt_ext">
-			<div class="how_bloctxt_int col-xs-offset-0 col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" id="howp_bltxt">
+			<div class="how_bloctxt_int col-xs-offset-0 col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8" id="howp_bltxt">
 				<h2> Entrepreneurs<br>
 					<small class="text-muted">Je Donne 2 Heures, comment ça va marcher ?</small></h2>
 				<p>
 					<ul>
 						<li>L'inscription sur <span>Je Donne 2 Heures</span> est gratuite.</li><br>
-						<li>Vous accédez dés votre arrivée à un espace personnel dédié afin de présenter librement votre projet ou votre entreprise (vidéo, photos et textes).</li></br>
-						<li>Cet espace vous sert également à proposer vos projets et donc à solliciter l'aide du grand public.<br><span>Je Donne 2 Heures</span> vous offre votre premier projet. Par la suite différentes formules payantes vous serons proposées.</li></br>
+						<li>Vous accédez dès votre arrivée à un espace personnel dédié, afin de présenter librement votre projet ou votre entreprise (vidéo, photos et textes).</li></br>
+						<li>Cet espace vous sert également à proposer vos projets et donc à solliciter l'aide du grand public.<br><span>Je Donne 2 Heures</span> vous offre votre premier projet. Par la suite, différentes formules payantes vous serons proposées.</li></br>
 						<li>Grâce à un processus de matching sécurisé, <span>Je Donne 2 Heures</span> vous met en relation avec les personnes qui désirent vous offrir de leur temps libre pour faire aboutir vos projets.</li></br>
 					</ul>
 				</p>
