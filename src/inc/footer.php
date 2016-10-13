d37d35771bcd70a11b8c439da2f20b367435b7de
Alexandre FRADET <a_fradet@yahoo.fr>
2016-10-13 11:02:33 +0200

    Correct some div height

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -2,13 +2,13 @@
 
     <div class="container-fluid center-block" id="foo_list">
         <div class="row visible-xs visible-sm foo_social">
-            <a href="https://www.facebook.com/jedonne2H">
+            <a href="https://www.facebook.com/jedonne2H" target="_blank">
                 <img src="../public/img/facebook.png" alt="facebook">
             </a>
-            <a href="https://twitter.com/jedonne2h">
+            <a href="https://twitter.com/jedonne2h" target="_blank">
                 <img  src="../public/img/twitter.png" alt="twitter">
             </a>
-            <a href="https://fr.linkedin.com/in/jd-2h-45318312b">
+            <a href="https://fr.linkedin.com/in/jd-2h-45318312b" target="_blank">
                 <img  src="../public/img/linkedin.png" alt="linkedin">
             </a>
         </div>
