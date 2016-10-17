9a8ea36be33d6780a135ec22d61b2c4e3dd5720e
Alexandre FRADET <a_fradet@yahoo.fr>
2016-10-17 17:27:45 +0200

    First commit, most of the migration done. Pages should be fine aside from small mistake. Controller is ok. Link between pages are done. 404 customized page done

--- src/Jd2hBundle/Controller/FrontController.php
+++ src/Jd2hBundle/Controller/FrontController.php
@@ -5,14 +5,30 @@ namespace Jd2hBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class FrontController extends Controller
-{
+class FrontController extends Controller {
+
     public function homepageAction()
-{
-    return $this->render('front/homepage.html.twig');
-}
+        {
+            return $this->render('front/homepage.html.twig');
+        }
+
     public function quisommesnousAction()
-{
-    return $this->render('front/quisommesnous.html.twig');
-}
+        {
+            return $this->render('front/quisommesnous.html.twig');
+        }
+
+    public function jedonneAction()
+        {
+            return $this->render('front/jedonne.html.twig');
+        }
+
+    public function jeproposeAction()
+        {
+            return $this->render('front/jepropose.html.twig');
+        }
+
+    public function errorAction()
+        {
+            return $this->render('front/error404.html.twig');
+        }
 }
