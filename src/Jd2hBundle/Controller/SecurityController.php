4e5c391590ae4e31ce7f2b64e1a83ae8eb41d209
Matthieu Janvier <janvierm@gmail.com>
2016-10-25 10:15:26 +0200

    login form instead of popup

--- src/Jd2hBundle/Controller/SecurityController.php
+++ src/Jd2hBundle/Controller/SecurityController.php
@@ -8,13 +8,26 @@
 
 namespace Jd2hBundle\Controller;
 
-
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends Controller
 {
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('back/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
     public function adminAction()
     {
         return $this->render('back/consultation.html.twig');
